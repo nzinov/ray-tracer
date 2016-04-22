ff2348568ba0a94069a966ebaa4fee26242c7131
Nikolay Zinov <nzinov@gmail.com>
2016-04-22 20:59:21 +0300

    add triangle primitive

---
+++ triangle.hpp
@@ -0,0 +1,42 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+#include "primitive.hpp"
+
+class Triangle : public Primitive {
+    Point a, b, c;
+public:
+    Triangle(Color color, Point a, Point b, Point c) : Primitive(color), a(a), b(b), c(c) {}
+    virtual Vector normal(Point) const {
+        return vec(b - a, c - a);
+    }
+
+    virtual Maybe<double> intersect(Ray ray) const {
+        Vector edge1 = b - a;
+        Vector edge2 = c - a;
+        //Begin calculating determinant - also used to calculate u parameter
+        Vector P = vec(ray.direction, edge2);
+        double det = dot(P, edge1);
+        if (almost_zero(det)) {
+            return Maybe<double>();
+        }
+        double inv_det = 1.0 / det;
+        Vector move = a - ray.start;
+
+        double u = dot(move, P) * inv_det;
+        if (u < 0.0 || u > 1.0) {
+            return Maybe<double>();
+        }
+        Vector Q = vec(move, edge1);
+        double v = dot(ray.direction, Q) * inv_det;
+        if(v < 0.0 || u + v  > 1.0) {
+            return Maybe<double>();
+        }
+        double t = dot(edge2, Q) * inv_det;
+        return t;
+    }
+
+    virtual Box bbox() const {
+        return Box(Point(), 0, 0, 0);
+    }
+};
+#endif
